21ac027dfc8ba3cc10c9703dda63bf8a151b5844
Ricky JH Liu <ricky.jh.liu@wnc.com.tw>
2022-04-12 17:57:43 +0800

    Remove unnecessary files

--- BlazorExcelViewer/Program.cs
+++ BlazorExcelViewer/Program.cs
@@ -1,4 +1,3 @@
-using BlazorExcelViewer.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 
@@ -7,7 +6,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddAntDesign();
 
 var app = builder.Build();
